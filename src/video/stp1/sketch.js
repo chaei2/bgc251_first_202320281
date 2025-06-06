4d369c7ca7b3d32f2011703bf69bb14214cdf4f1
chaei <209975817+chaei2@users.noreply.github.com>
2025-06-06 10:57:24 +0900

    video_stp1

---
+++ src/video/stp1/sketch.js
@@ -0,0 +1,33 @@
+let video;
+
+function setup() {
+  createCanvas(800, 600);
+  video = createCapture(VIDEO, { flipped: true });
+  video.hide();
+  console.log(video);
+  video.size(64, 48);
+}
+
+function draw() {
+  // background(220);
+
+  let aspectRatioCanvas = width / height;
+  let aspectRatioVideo = video.width / video.height;
+  let newWidth, newHeight;
+  let zeroX, zeroY;
+
+  if (aspectRatioCanvas > aspectRatioVideo) {
+    newWidth = width;
+    newHeight = newWidth / aspectRatioVideo;
+  } else {
+    newHeight = height;
+    newWidth = newHeight * aspectRatioVideo;
+  }
+  zeroX = (width - newWidth) / 2;
+  zeroY = (height - newHeight) / 2;
+
+  image(video, zeroX, zeroY, newWidth, newHeight);
+  noStroke();
+  fill('red');
+  circle(mouseX, mouseY, 50);
+}
