f002c5105c26de42e9527bf4601348a1db978e9e
chaei <209975817+chaei2@users.noreply.github.com>
2025-06-13 09:22:40 +0900

    혹시몰라 다시 수정한 stp5+ 이해

--- src/final_project/stp5/sketch.js
+++ src/final_project/stp5/sketch.js
@@ -36,6 +36,7 @@ function setup() {
 
   // faceMesh 초기화 (ml5@0.6.0 방식)
   // faceMesh = ml5.faceMesh({ maxFaces: 1, flipped: true })이게 안되네..
+  // faceMash 웹 캠 적용
   faceMesh = ml5.facemesh(video, () => {
     console.log('faceMesh ready');
   });
@@ -60,6 +61,8 @@ function draw() {
   lipStemp.rect(0, 0, width, height);
 
   // 손교수님 수업 video stp1 부분 cover()
+  // 비디오 비율 맞추기
+
   let aspectRatioCanvas = width / height;
   let aspectRatioVideo = video.width / video.height;
 
@@ -81,14 +84,19 @@ function draw() {
   // cover로 인한 비율 문제로 얼굴 점도 비율에 맞춘 코드(정상화)
   if (faces.length > 0) {
     let face = faces[0];
+    // 키 포인트들을 얼굴 스케일
     let keypoints = face.scaledMesh;
 
     // 왜 안되냐..
     // 입술 좌표들..입술 왜 하란대로 했는데.. 안나오냐 입술 숫자 다니엘 데니쉬분 보면서 적음..주댕이..힘드네
+
+    // 주딩이 밖 값들
     let lipsExterior = [
       267, 269, 270, 409, 291, 375, 321, 405, 314, 17, 84, 181, 91, 146, 61,
       185, 40, 39, 37, 0,
     ];
+
+    // 주딩이 안 값들
     let lipsInterior = [
       13, 312, 311, 310, 415, 308, 324, 318, 402, 317, 14, 87, 178, 88, 95, 78,
       191, 80, 81, 82,
@@ -99,9 +107,12 @@ function draw() {
 
     // 주댕이 색
     // if (frameCount % 45 === 0) {
+    // 중심좌표 계산
     let cX = 0,
       cY = 0;
     for (let centers = 0; centers < lipsExterior.length; centers++) {
+      // 좌표는 키 포인트[립의 계산 값을 가지는 [조건을 달성한 숫자들]] 키 포인트들의 일부인 립 바깥에
+
       cX += keypoints[lipsExterior[centers]][0];
       cY += keypoints[lipsExterior[centers]][1];
     }
@@ -115,11 +126,17 @@ function draw() {
     lipStemp.beginShape();
 
     for (let aSum = 0; aSum < lipsExterior.length; aSum++) {
+      // 좌표는 키 포인트[립의 계산 값을 가지는 [해당 조건을 달성한 숫자들]]
       let pt = keypoints[lipsExterior[aSum]];
+      // x의 위치는 () * 크기 + 해당 숫자
+      // pt[0] - cX: 각 점에서 중심까지의 거리
+      // 각점 중심까지 거리 * 배수 + 해당 거리 더 가기
       let x = (pt[0] - cX) * lipBig + cX;
       let y = (pt[1] - cY) * lipBig + cY;
       x = map(x, 0, video.width, zeroX, zeroX + newWidth);
       y = map(y, 0, video.height, zeroY, zeroY + newHeight);
+
+      // vertex()는 지정 점들로 도형 만들기 위해 사용함
       lipStemp.vertex(x, y);
     }
 
@@ -127,6 +144,7 @@ function draw() {
     lipStemp.beginContour();
 
     // 꼭 입술 안에 색 없애려면 반대로 순회하는 구조 만들어야함
+    // 기존 구조에서 반대로 가게 만들면 (시계 반향 반대로) 애가 뚫리는 구조로 인식
     for (let cnt = lipsInterior.length - 1; cnt >= 0; cnt--) {
       let pt = keypoints[lipsInterior[cnt]];
       let x = (pt[0] - cX) * lipBig + cX;
@@ -135,6 +153,7 @@ function draw() {
       y = map(y, 0, video.height, zeroY, zeroY + newHeight);
       lipStemp.vertex(x, y);
     }
+    // endContour()입술 구멍 만들떄 필요함
     lipStemp.endContour();
 
     lipStemp.endShape(CLOSE);
